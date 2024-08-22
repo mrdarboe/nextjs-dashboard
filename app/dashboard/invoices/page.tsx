e04375765a3d5d2ae1c5692b1e047f3962ac84d4
mrdarboe <gsdarboe1@gmail.com>
2024-08-22 10:06:02 +0000

    create invoice

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -26,10 +26,10 @@ export default async function Page({
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
         <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
+        <CreateInvoice />
       </div>
       <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
         <Search placeholder="Search invoices..." />
-        <CreateInvoice />
       </div>
        <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
         <Table query={query} currentPage={currentPage} />
