921da2c1a14cd4318b8fcae7f8db147518058262
mrdarboe <gsdarboe1@gmail.com>
2024-08-26 07:51:14 +0000

    edit-form validation

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -63,13 +63,19 @@ export async function createInvoice(prevState: State, formData: FormData) {
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
   
-export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(id: string, prevState: State, formData: FormData) {
+  const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
- 
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Cannot Remove Required Fields. Failed to Update Invoice.',
+    };
+  }
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
 try {
   await sql`
